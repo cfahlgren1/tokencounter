b261d7283c2ae1e54a035a757d680218c5016561
Caleb Fahlgren <calebfahlgren@protonmail.com>
2023-07-24 16:24:35 -0500

    init commit

---
+++ src/components/Layout.js
@@ -0,0 +1,42 @@
+import { NextSeo } from 'next-seo';
+import Navbar from './Navbar';
+const Layout = ({ children, title, description, url, imageUrl }) => {
+    return (
+        <>
+            <NextSeo
+                title={title}
+                description={description}
+                openGraph={{
+                    url: url,
+                    title: title,
+                    description: description,
+                    images: [
+                        {
+                            url: imageUrl,
+                            width: 800,
+                            height: 600,
+                            alt: 'TokenCounterLogo',
+                            type: 'image/png',
+                        }
+                    ],
+                    siteName: 'TokenCounter',
+                }}
+                twitter={{
+                    handle: '@thecalebf',
+                    site: '@site',
+                    cardType: 'summary_large_image',
+                }}
+                additionalLinkTags={[
+                    {
+                        rel: 'icon',
+                        href: '/favicon.ico',
+                    },
+                ]}
+            />
+            <Navbar />
+            <main>{children}</main>
+        </>
+    );
+}
+
+export default Layout;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,118 +1,148 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import { useState, useMemo } from "react";
+import { encode, decode } from 'gpt-tokenizer/model/gpt-3.5-turbo';
+import Layout from "@/components/Layout";
 
-const inter = Inter({ subsets: ['latin'] })
+const MODEL_COST_PER_1K_TOKENS = {
+  "gpt-4": 0.03,
+  "gpt-4-32k": 0.06,
+  "gpt-4-completion": 0.06,
+  "gpt-4-32k-completion": 0.12,
+  "gpt-3.5-turbo": 0.0015,
+  "gpt-3.5-turbo-16k": 0.003,
+  "gpt-3.5-turbo-completion": 0.002,
+  "gpt-3.5-turbo-16k-completion": 0.004,
+  "text-ada-001": 0.0004,
+  "ada": 0.0004,
+  "text-babbage-001": 0.0005,
+  "babbage": 0.0005,
+  "text-curie-001": 0.002,
+  "curie": 0.002,
+  "text-davinci-003": 0.02,
+  "text-davinci-002": 0.02,
+  "code-davinci-002": 0.02,
+  "ada-finetuned": 0.0016,
+  "babbage-finetuned": 0.0024,
+  "curie-finetuned": 0.012,
+  "davinci-finetuned": 0.12,
+};
 
-export default function Home() {
+const models = [
+  "gpt-3.5-turbo",
+  "gpt-3.5-turbo-16k",
+  "gpt-3.5-turbo-completion",
+  "gpt-3.5-turbo-16k-completion",
+  "gpt-4",
+  "gpt-4-32k",
+  "gpt-4-completion",
+  "gpt-4-32k-completion",
+  "text-davinci-003",
+  "text-ada-001",
+  "ada",
+  "text-babbage-001",
+  "babbage",
+  "text-curie-001",
+  "curie",
+  "text-davinci-002",
+  "code-davinci-002",
+  "ada-finetuned",
+  "babbage-finetuned",
+  "curie-finetuned",
+  "davinci-finetuned",
+];
+
+const colors = ['bg-blue-200', 'bg-red-200', 'bg-green-200', 'bg-yellow-200', 'bg-purple-200', 'bg-pink-200'];
+
+const TokenizerPage = () => {
+  const exampleText = `Some words become one token, but others, like "indivisible", don't. \nEmojis like 🤚🏾 can turn into many tokens. \n\nCharacters often seen together, like "1234567890", may become one token.`;
+  const [inputText, setInputText] = useState(exampleText);
+  const [selectedModel, setSelectedModel] = useState(models[0]);
+
+  const encodedTokens = useMemo(() => {
+    const result = encode(inputText);
+    return Array.isArray(result) ? result : [];
+  }, [inputText]);
+
+  const setExampleContent = () => {
+    setInputText(exampleText);
+  };
+
+  const decodedTokens = useMemo(() => Array.isArray(encodedTokens) ? encodedTokens.map(token => decode([token])) : [], [encodedTokens]);
+  const colorTokens = useMemo(() => decodedTokens.map((_, i) => colors[i % colors.length]), [decodedTokens]);
+  const cost = useMemo(() => (encodedTokens.length / 1000) * (MODEL_COST_PER_1K_TOKENS[selectedModel] || 0), [encodedTokens, selectedModel]);
+
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <>
+      <Layout>
+        <div className="mx-auto px-5 py-10 max-w-3xl">
+          <h2 className="text-4xl font-bold mb-5">GPT Tokenizer</h2>
+          <h4 className="text-xl mb-5">Estimate GPT costs and token count from your text!</h4>
+          <p>Tokens are groups of characters often seen together. These models can predict what token comes next in a sequence, making them great at generating text.</p>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+          <div className="my-5">
+            <select
+              value={selectedModel}
+              onChange={(e) => setSelectedModel(e.target.value)}
+              class="text-md select select-bordered w-full max-w-xs"
+            >
+              {models.map((model) => (
+                <option key={model} value={model}>
+                  {model}
+                </option>
+              ))}
+            </select>
+          </div>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+          <textarea
+            value={inputText}
+            onChange={(e) => setInputText(e.target.value)}
+            className="textarea textarea-success form-textarea block w-full text-lg p-1 mb-5 border-2 border-gray-300 rounded h-64"
+          />
+          <div className="flex">
+            <button
+              onClick={setExampleContent}
+              className="btn mx-1 btn-md text-sm"
+            >
+              Show Example
+            </button>
+            <button
+              onClick={() => setInputText("")}
+              className="btn mx-1 btn-md text-sm"
+            >
+              Clear
+            </button>
+          </div>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+          {/* stats */}
+          <div className="mt-10 grid grid-cols-3 gap-6 text-center">
+            <div>
+              <div className="text-gray-500">Number of Characters</div>
+              <div className="text-3xl">{inputText.length}</div>
+            </div>
+            <div>
+              <div className="text-gray-500">Number of Tokens</div>
+              <div className="text-3xl">{encodedTokens.length}</div>
+            </div>
+            <div>
+              <div className="text-gray-500">Estimated Cost</div>
+              <div className="text-3xl">${cost.toFixed(6)}</div>
+            </div>
+          </div>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
-}
+          <div className="border-2 my-10 border-gray-200 bg-gray-200 rounded p-2 h-32 overflow-auto">
+            {decodedTokens.map((token, i) => (
+              <span
+                key={i}
+                className={`inline-block rounded px-1 mb-1 ${colorTokens[i]}`}
+              >
+                {token}
+              </span>
+            ))}
+          </div>
+        </div >
+      </Layout>
+    </>
+  );
+};
+
+export default TokenizerPage;
